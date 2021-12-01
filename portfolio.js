f9b775506d6f1e1429834add2cfa0f388685bd1d
Shubhankar <shubhosen98@gmail.com>
2021-12-01 20:43:07 +0530

    Made some changes to the contact form regarding input fields

--- portfolio.js
+++ portfolio.js
@@ -2,8 +2,10 @@
 
 const hamburger_button = document.querySelector('.navbar-toggler');
 const icon_toggler = document.querySelector('.navbar-toggler-icon');
-const navbar_anchors =  document.querySelectorAll('#navhelp .nav-link');
-
+const navbar_anchors = document.querySelectorAll('#navhelp .nav-link');
+const inp_element = document.querySelectorAll('input');
+const text_area = document.querySelector('textarea');
+const btn = document.querySelector('.contact-pg-btn-form');
 
 
 // function declaration
@@ -29,3 +31,41 @@ hamburger_button.addEventListener('click', function toggle(evt) {
 })
 
 
+//Example starter JavaScript for disabling form submissions if there are invalid fields
+
+
+function main() {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+}
+
+// executing the function to disable if fields not filled and that too with validity as provided.
+main(); 
+
+
+// resists the form from the default behaviour of submission when inside a form.
+
+
+// try {
+//     btn.addEventListener('click', (evt) => {
+//         evt.preventDefault(); // method on the event object.
+//     })
+// }
+
+// catch (err) {
+// }
